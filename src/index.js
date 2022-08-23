f47dd0bcfd56f3ab8577771c94e14278951bd4b9
+ <+>
2022-08-23 13:41:44 +0300

    done assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleTodo(e.target.new_task.value);
+    form.reset();
+  })
 });
+
+function handleTodo(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  let listItem = document.createElement('li');
+  btn.textContent = 'x';
+  btn.addEventListener('click', handleDelete);
+  p.textContent = `${todo} `;
+    p.appendChild(btn)
+  listItem.appendChild(p);
+  document.querySelector('#tasks').appendChild(listItem);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.parentNode.remove();
+  
+}
